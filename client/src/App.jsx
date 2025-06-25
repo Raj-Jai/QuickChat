e47fab081b37c80c89b6e526ef96276b6ef77a1b
raj-jai <jairaj0616@gmail.com>
2025-06-26 04:34:57 +0530

    added onrender in socket

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect, useRef } from 'react';
 import io from 'socket.io-client';
 import './App.css';
 
-const socket = io('http://localhost:4000');
+const socket = io('https://quickchat-78zv.onrender.com/');
 
 function App() {
   const [username, setUsername] = useState('');
